13ad96397d0731ed605496f3da7ae2ebab94f191
yzarra <glasberg.catona@gmail.com>
2026-06-07 15:20:51 -0400

    fixed links

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -32,7 +32,7 @@ function Register({ onRegister, onSwitch }) {
 
     // calls register method in server
     try {
-      const res = await fetch('https://appliance-inventory-web.onrender.com', {
+      const res = await fetch('https://appliance-inventory-web.onrender.com/api/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
